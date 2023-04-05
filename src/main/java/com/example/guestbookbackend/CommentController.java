eaef1dbb45a5fb558276ebf6afe4469102d74e55
Conor <conorcl@hotmail.com>
2023-04-04 19:05:20 -0700

    all

--- src/main/java/com/example/guestbookbackend/CommentController.java
+++ src/main/java/com/example/guestbookbackend/CommentController.java
@@ -11,6 +11,10 @@ public class CommentController {
 
     private final MySQLCommentDao mySQLCommentDao;
 
+
+    @Value("${spring.datasource.url}")
+    private String dbHost;
+
     public CommentController(MySQLCommentDao mySQLCommentDao) {
         this.mySQLCommentDao = mySQLCommentDao;
     }
@@ -29,5 +33,11 @@ public class CommentController {
     public ResponseEntity<Iterable<Comment>> getComments(){
         var comments = mySQLCommentDao.getComments();
         return ResponseEntity.ok(comments);
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping(value = "hi")
+    public ResponseEntity<String> getHi(){
+        return ResponseEntity.ok(dbHost);
     }
 }
