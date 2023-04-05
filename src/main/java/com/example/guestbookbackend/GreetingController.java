eefb32588170b8a9e2bb882c6dcca08fdec9b886
Conor <conorcl@hotmail.com>
2023-04-04 19:41:49 -0700

    update version

---
+++ src/main/java/com/example/guestbookbackend/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.guestbookbackend;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class GreetingController {
+
+
+
+
+    @CrossOrigin(origins = "*")
+    @GetMapping(value = "greeting")
+    public ResponseEntity<String> getComments(){
+        return ResponseEntity.ok("greeeting");
+    }
+}
